2bc44632cb5a0ae1d80b27ffccbc22261b8fdc15
noglows <jessica.noglows@outlook.com>
2016-01-28 12:28:37 -0800

    Working game over logic

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,23 +1,19 @@
 function TicTacToe() {
-//  var board_spaces = ["r1c1", "r1c2", "r1c3", "r2c1", "r2c2", "r2c3", "r3c1", "r3c2", "r3c3"];
-  //var player = 0;
-
   this.pickSpaces();
   this.resetBoard();
-
 }
 
 TicTacToe.prototype = {
-  board: [["", "", ""], ["", "", ""], ["", "", ""]],
-  choices: ["", "X", "O"],
+  // board: [["", "", ""], ["", "", ""], ["", "", ""]],
+  // choices: ["", "X", "O"],
   board_spaces: ["r1c1", "r1c2", "r1c3", "r2c1", "r2c2", "r2c3", "r3c1", "r3c2", "r3c3"],
+
   pickSpaces: function(){
+    self = this;
     var player = 0;
-    console.log("Yes Jessica!");
-    this.board_spaces.forEach(function(box) {
+    self.board_spaces.forEach(function(box) {
       $("." + box).click(function() {
         if ($("." + box).css('background-color') == 'rgb(0, 0, 255)') {
-          console.log(player);
           if(player === 0){
             $("." + box).css('background-color', 'red');
             player = 1;
@@ -26,19 +22,35 @@ TicTacToe.prototype = {
             player = 0;
           }
         }
+        self.gameOver();
       });
     });
+
   },
   resetBoard: function(){
+    self = this;
     $('#reset-button').click(function() {
-      board_spaces = ["r1c1", "r1c2", "r1c3", "r2c1", "r2c2", "r2c3", "r3c1", "r3c2", "r3c3"];
-      //console.log(this.board_spaces);
-      board_spaces.forEach(function(box) {
+      self.board_spaces.forEach(function(box) {
         $("." + box).css('background-color', 'blue');
     });
   });
+},
+boardSpaces: function() {
+  return ["r1c1", "r1c2", "r1c3", "r2c1", "r2c2", "r2c3", "r3c1", "r3c2", "r3c3"];
+},
+
+gameOver: function() {
+  self = this;
+  var count = 0;
+  self.board_spaces.forEach(function(box) {
+    if ($("." + box).css('background-color') == 'rgb(0, 0, 255)') {
+      count++;
+    }
+  });
+  if (count === 0){
+    alert("Game Over!");
   }
+},
+
 
 };
-
-//tictactoe = new TicTacToe();
