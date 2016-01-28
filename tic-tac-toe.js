8fb51ff84b5a50e18728b3013aa25ac15fcaaabb
Jennie Buechner <janina.buechner@gmail.com>
2016-01-28 09:46:57 -0800

    init gameboard and create make move method

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,7 +1,15 @@
 function TicTacToe() {
-
+	this.board = [[0,0,0],[0,0,0],[0,0,0]];
 }
 
-TicTacToe.prototype = {
-  
-}
+TicTacToe.prototype.start = function() {
+		console.log('Game has started');
+};
+
+TicTacToe.prototype.printGameboard = function() {
+
+};
+
+TicTacToe.prototype.makeMove = function(row, col) {
+	this.board[row][col] = 1;
+};
