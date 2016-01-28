c13ff14aa85e8086ce78f02cc9ebfd8ba4659584
Kelly <kelly_devlin@alumni.brown.edu>
2016-01-28 10:37:48 -0800

    initial board setup

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,7 +1,23 @@
 function TicTacToe() {
-
+  this.board = [[0, 0, 0],
+                [0, 0, 0],
+                [0, 0, 0]
+                ];
+  this.current_player = 1;
 }
 
-TicTacToe.prototype = {
-  
-}
+TicTacToe.prototype.play_turn = function(row, column) {
+  this.board[row][column] = this.current_player;
+  if (this.current_player === 1) {
+    this.current_player = 2;
+  }
+  else {
+    this.current_player = 1;
+  }
+};
+
+$(document).ready(function() {
+
+
+
+});
