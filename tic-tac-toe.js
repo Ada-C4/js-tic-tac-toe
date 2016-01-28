696c41eac9b542b981a207f23f181930b40b832e
Jennie Buechner <janina.buechner@gmail.com>
2016-01-28 11:28:54 -0800

    add click event to each square

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -15,14 +15,16 @@ TicTacToe.prototype.start = function() {
 TicTacToe.prototype.printGameboard = function() {
 	for (var row = 0; row < this.board.length; row++) {
 		for (var col = 0; col < this.board[0].length; col++) {
-			var $square = $('#row' + row + 'col' + col),
+			var $square = $('#row' + row + 	'col' + col),
 					squareNumber = this.board[row][col];
 			$square.children('img').attr('src', this.images[squareNumber]);
 		}
 	}
 };
 
-TicTacToe.prototype.makeMove = function(row, col) {
+TicTacToe.prototype.makeMove = function(el) {
+	var col = el.data().col,
+			row = el.data().row;
 	if (this.board[row][col] === 0) {
 		this.board[row][col] = this.currentPlayer;
 		this.currentPlayer = this.currentPlayer === 1? 2 : 1;	
