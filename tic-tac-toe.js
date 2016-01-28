c546346121f900be309c096ffb3bf19f5b7f7911
Dreedle <davis.audrey.lynn@gmail.com>
2016-01-28 10:43:27 -0800

    Adding images and classes for stormtrooper and vader

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -2,6 +2,36 @@ function TicTacToe() {
 
 }
 
+
 TicTacToe.prototype = {
-  
+  this.gameLogic = function () {
+    this.new = function () {
+      //make a new blank board
+      //reset things that may need to be reset
+    };
+
+
+    this.board = function () {
+      var board = [[0, 0 ,0], [0, 0, 0], [0, 0, 0]];
+
+    }
+
+    this.endgame = function () {
+      //ends the game
+    }
+
+    this.gameover? = function () {
+      //checks to see if the game is over
+      //calls endgame if it is
+    }
+  }
+
+  this.gameDisplay = function () {
+    this.finalDisplay = function () {
+      //the game has ended, and endgame calls this method to
+      //show a message at the end
+    }
+
+  }
+
 }
