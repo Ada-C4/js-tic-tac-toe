dcf8dbbefae0ee7935e9425205d1a34bd9c016db
Jenna Nichols <jennaplusplus@gmail.com>
2016-01-28 14:04:57 -0800

    alternate placement of X and O

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,24 +1,25 @@
 function TicTacToe() {
   this.board = [[0,0,0], [0,0,0], [0,0,0]];
-  this.playerOne = '<p>X</p>';
-  this.playerTwo = '<p>O</p>';
+  this.playerOne = 'X';
+  this.playerTwo = 'O';
   this.currentPlayer = this.playerOne;
 }
 
 TicTacToe.prototype.play = function() {
-  var i = 0;
-  while (i < 9) {
-    if (i % 2 === 0) {
-      this.takeTurn(this.playerOne);
-    } else {
-      this.takeTurn(this.playerTwo);
-    }
-    i++;
-  }
+  this.takeTurn(this.currentPlayer);
 };
 
 TicTacToe.prototype.takeTurn = function(player) {
-  $(".square").click(function () {
-    $(this).append(player);
-  });
+  // update board attributes
+  // figure out if someone won
+
+  this.switchPlayer();
+};
+
+TicTacToe.prototype.switchPlayer = function () {
+  if (this.currentPlayer === this.playerOne) {
+    this.currentPlayer = this.playerTwo;
+  } else {
+    this.currentPlayer = this.playerOne;
+  }
 };
