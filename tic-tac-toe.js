b372c76e56c232b2e0121f942a1151cd49bc49fc
Brittany <BrittanyKohler@gmail.com>
2016-01-28 12:17:36 -0800

    adding logic to update board array

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,28 +1,36 @@
 function TicTacToe() {
-  this.x = true;
+  this.player1 = true;
   this.board = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
-  this.board_spaces = ["r1c1", "r1c2", "r1c3", "r2c1", "r2c2", "r2c3", "r3c1", "r3c2", "r3c3"];
+  this.board_spaces = ["r0c0", "r0c1", "r0c2", "r1c0", "r1c1", "r1c2", "r2c0", "r2c1", "r2c2"];
 }
 
-
-TicTacToe.prototype.addSymbolOnClick = function() {
+TicTacToe.prototype.playTurn = function() {
   var self = this;
   this.board_spaces.forEach(function(space) {
     $("#" + space).click(function() {
       $("#" + space).text(self.getSymbol());
       self.changePlayer();
+      self.updateBoard(this);
     });
   });
 };
 
+TicTacToe.prototype.updateBoard = function(cell) {
+  row = (cell.id[1]);
+  column = (cell.id[3]);
+  console.log(row, column);
+  this.board[row][column] = this.getSymbol();
+  console.log(this.board);
+}
+
 TicTacToe.prototype.changePlayer = function() {
-  this.x = !this.x;
+  this.player1 = !this.player1;
 };
 
 TicTacToe.prototype.getSymbol = function() {
-  return this.x ? "X" : "O";
+  return this.player1 ? "X" : "O";
 };
 
 TicTacToe.prototype.play = function() {
-  this.addSymbolOnClick();
+  this.playTurn();
 };
