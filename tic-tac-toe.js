66aee0f0d7511ae20a8c27d04ee6fc6f04b0f5f8
noglows <jessica.noglows@outlook.com>
2016-01-28 10:50:06 -0800

    Two different player colors

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,12 +1,18 @@
 function TicTacToe() {
-  var board_2 = ["r1c1", "r1c2", "r1c3", "r2c1", "r2c2", "r2c3", "r3c1", "r3c2", "r3c3"];
-  board_2.forEach(function(box) {
+  var board_spaces = ["r1c1", "r1c2", "r1c3", "r2c1", "r2c2", "r2c3", "r3c1", "r3c2", "r3c3"];
+  var player = 0;
+  board_spaces.forEach(function(box) {
     $("." + box).click(function() {
-      $("." + box).css('background-color', 'red');
+      if(player === 0){
+        $("." + box).css('background-color', 'red');
+        player = 1;
+      } else {
+        $("." + box).css('background-color', 'yellow');
+        player = 0;
+      }
     });
   });
-
-  }
+}
 
 TicTacToe.prototype = {
   board: [["", "", ""], ["", "", ""], ["", "", ""]],
