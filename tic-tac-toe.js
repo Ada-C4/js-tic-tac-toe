b52286826bbc183f31512f7efeb5c5d560b80750
Dreedle <davis.audrey.lynn@gmail.com>
2016-01-28 11:23:40 -0800

    Can begin a game

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,33 +1,32 @@
-function TicTacToe() {
-
+var TicTacToe = function () {
 }
 
-TicTacToe.prototype = function () {
-    this.begin = function () {
-    };
+TicTacToe.prototype = {
+    begin: function () {
+      console.log("game has begun");
+    }
+}
 
-
-    this.board = function () {
+TicTacToe.prototype.board = function () {
       var board = [[0, 0 ,0], [0, 0, 0], [0, 0, 0]];
-
     };
 
-    this.endgame = function () {
-      //ends the game
-    };
 
-    this.gameover = function () {
-      //checks to see if the game is over
-      //calls endgame if it is
-    };
-
-    this.finalDisplay = function () {
-      //the game has ended, and endgame calls this method to
-      //show a message at the end
-    };
-
-    this.markSquare = function () {
-
-    };
-
-}
+    // this.endgame = fu
+    // nction () {
+    //   //ends the game
+    // };
+    //
+    // this.gameover = function () {
+    //   //checks to see if the game is over
+    //   //calls endgame if it is
+    // };
+    //
+    // this.finalDisplay = function () {
+    //   //the game has ended, and endgame calls this method to
+    //   //show a message at the end
+    // };
+    //
+    // this.markSquare = function () {
+    //
+    // };
