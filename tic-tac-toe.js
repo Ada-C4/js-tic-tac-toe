2c192cd7225044cebd405b926e628827805228ee
emgord <emgord@gmail.com>
2016-01-28 11:29:16 -0800

    draw board method added

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,7 +1,22 @@
 function TicTacToe() {
-  this.game = [[0,0,0],[0,0,0],[0,0,0]];
+  this.board = [[0,0,0],[0,0,0],[0,0,0]];
 }
 
-TicTacToe.prototype = {
-
+TicTacToe.prototype.drawBoard = function() {
+  var row = $(".row");
+  for (var i=0; i < this.board.length; i++) {
+    var squares = $(row[i]).children();
+    var content = this.board[i];
+    for (var i2=0; i2 < content.length; i2++) {
+      if (content[i2] === "X") {
+        $(squares[i2]).append("<p>X</p>");
+      } else if (content[i2] === "O") {
+        $(squares[i2]).append("<p>O</p>");
+      }
+    }
+  }
 };
+
+TicTacToe.prototype.playTurn = function(){
+  
+}
