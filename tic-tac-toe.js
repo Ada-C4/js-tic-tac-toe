e51a760042fe2d1e2b6274a0850407981ff4ce9b
Jenna Nichols <jennaplusplus@gmail.com>
2016-01-28 12:02:15 -0800

    some imperfect game logic to place Xs and Os

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,7 +1,24 @@
 function TicTacToe() {
-
+  this.board = [[0,0,0], [0,0,0], [0,0,0]];
+  this.playerOne = '<p>X</p>';
+  this.playerTwo = '<p>O</p>';
+  this.currentPlayer = this.playerOne;
 }
 
-TicTacToe.prototype = {
-  
-}
+TicTacToe.prototype.play = function() {
+  var i = 0;
+  while (i < 9) {
+    if (i % 2 === 0) {
+      this.takeTurn(this.playerOne);
+    } else {
+      this.takeTurn(this.playerTwo);
+    }
+    i++;
+  }
+};
+
+TicTacToe.prototype.takeTurn = function(player) {
+  $(".square").click(function () {
+    $(this).append(player);
+  });
+};
