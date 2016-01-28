d3a50f3a03c11633162ccb912b1950f4b969e60c
noglows <jessica.noglows@outlook.com>
2016-01-28 12:30:31 -0800

    Cleaned up the code a little bit

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -27,30 +27,30 @@ TicTacToe.prototype = {
     });
 
   },
+
   resetBoard: function(){
     self = this;
     $('#reset-button').click(function() {
       self.board_spaces.forEach(function(box) {
         $("." + box).css('background-color', 'blue');
+      });
     });
-  });
-},
-boardSpaces: function() {
-  return ["r1c1", "r1c2", "r1c3", "r2c1", "r2c2", "r2c3", "r3c1", "r3c2", "r3c3"];
-},
+  },
 
-gameOver: function() {
-  self = this;
-  var count = 0;
-  self.board_spaces.forEach(function(box) {
-    if ($("." + box).css('background-color') == 'rgb(0, 0, 255)') {
-      count++;
+  boardSpaces: function() {
+    return ["r1c1", "r1c2", "r1c3", "r2c1", "r2c2", "r2c3", "r3c1", "r3c2", "r3c3"];
+  },
+
+  gameOver: function() {
+    self = this;
+    var count = 0;
+    self.board_spaces.forEach(function(box) {
+      if ($("." + box).css('background-color') == 'rgb(0, 0, 255)') {
+        count++;
+      }
+    });
+    if (count === 0){
+      alert("Game Over!");
     }
-  });
-  if (count === 0){
-    alert("Game Over!");
-  }
-},
-
-
+  },
 };
