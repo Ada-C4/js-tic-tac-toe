2149d0d992b169a3737b525fb3c06e317f298a20
noglows <jessica.noglows@outlook.com>
2016-01-28 11:52:56 -0800

    Moved things into the prototype

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,39 +1,44 @@
 function TicTacToe() {
-  var board_spaces = ["r1c1", "r1c2", "r1c3", "r2c1", "r2c2", "r2c3", "r3c1", "r3c2", "r3c3"];
-  var player = 0;
-  var already_played = false;
+//  var board_spaces = ["r1c1", "r1c2", "r1c3", "r2c1", "r2c2", "r2c3", "r3c1", "r3c2", "r3c3"];
+  //var player = 0;
 
-  board_spaces.forEach(function(box) {
-    $("." + box).click(function() {
-      if ($("." + box).css('background-color') == 'rgb(0, 0, 255)') {
-        if(player === 0){
-          $("." + box).css('background-color', 'red');
-          player = 1;
-        } else {
-          $("." + box).css('background-color', 'yellow');
-          player = 0;
-        }
-      }
-    });
-  });
+  this.pickSpaces();
+  this.resetBoard();
 
-  $('#reset-button').click(function() {
-    board_spaces.forEach(function(box) {
-      $("." + box).css('background-color', 'blue');
-  });
-});
 }
 
 TicTacToe.prototype = {
   board: [["", "", ""], ["", "", ""], ["", "", ""]],
   choices: ["", "X", "O"],
-  //board_2: ["r1c1", "r1c2", "r1c3", "r2c1", "r2c2", "r2c3", "r3c1", "r3c2", "r3c3"]
+  board_spaces: ["r1c1", "r1c2", "r1c3", "r2c1", "r2c2", "r2c3", "r3c1", "r3c2", "r3c3"],
+  pickSpaces: function(){
+    var player = 0;
+    console.log("Yes Jessica!");
+    this.board_spaces.forEach(function(box) {
+      $("." + box).click(function() {
+        if ($("." + box).css('background-color') == 'rgb(0, 0, 255)') {
+          console.log(player);
+          if(player === 0){
+            $("." + box).css('background-color', 'red');
+            player = 1;
+          } else {
+            $("." + box).css('background-color', 'yellow');
+            player = 0;
+          }
+        }
+      });
+    });
+  },
+  resetBoard: function(){
+    $('#reset-button').click(function() {
+      board_spaces = ["r1c1", "r1c2", "r1c3", "r2c1", "r2c2", "r2c3", "r3c1", "r3c2", "r3c3"];
+      //console.log(this.board_spaces);
+      board_spaces.forEach(function(box) {
+        $("." + box).css('background-color', 'blue');
+    });
+  });
+  }
+
 };
 
-tictactoe = new TicTacToe();
-
-
-
-
-
-//:nth-child(n)	$("p:nth-child(2)")
+//tictactoe = new TicTacToe();
