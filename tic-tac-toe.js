b9374222663c283605503806bfdc9fa22bb9b227
noglows <jessica.noglows@outlook.com>
2016-01-28 10:57:41 -0800

    Boxes can not be changed once they are picked

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,14 +1,18 @@
 function TicTacToe() {
   var board_spaces = ["r1c1", "r1c2", "r1c3", "r2c1", "r2c2", "r2c3", "r3c1", "r3c2", "r3c3"];
   var player = 0;
+  var already_played = false;
+
   board_spaces.forEach(function(box) {
     $("." + box).click(function() {
-      if(player === 0){
-        $("." + box).css('background-color', 'red');
-        player = 1;
-      } else {
-        $("." + box).css('background-color', 'yellow');
-        player = 0;
+      if ($("." + box).css('background-color') == 'rgb(0, 0, 255)') {
+        if(player === 0){
+          $("." + box).css('background-color', 'red');
+          player = 1;
+        } else {
+          $("." + box).css('background-color', 'yellow');
+          player = 0;
+        }
       }
     });
   });
