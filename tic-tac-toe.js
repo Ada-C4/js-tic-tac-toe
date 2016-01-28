9e787c7ea1360d4ca9edc730f716d1c160cc3cea
Dreedle <davis.audrey.lynn@gmail.com>
2016-01-28 11:01:44 -0800

    Can make a new TicTacToe

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -2,36 +2,32 @@ function TicTacToe() {
 
 }
 
-
-TicTacToe.prototype = {
-  this.gameLogic = function () {
-    this.new = function () {
-      //make a new blank board
-      //reset things that may need to be reset
+TicTacToe.prototype = function () {
+    this.begin = function () {
     };
 
 
     this.board = function () {
       var board = [[0, 0 ,0], [0, 0, 0], [0, 0, 0]];
 
-    }
+    };
 
     this.endgame = function () {
       //ends the game
-    }
+    };
 
-    this.gameover? = function () {
+    this.gameover = function () {
       //checks to see if the game is over
       //calls endgame if it is
-    }
-  }
+    };
 
-  this.gameDisplay = function () {
     this.finalDisplay = function () {
       //the game has ended, and endgame calls this method to
       //show a message at the end
-    }
+    };
 
-  }
+    this.markSquare = function () {
+
+    };
 
 }
