79f0073f98fb9b4b883498ed04ed67b8419d1106
Wesley Willis <haileyw@gmail.com>
2016-01-28 13:55:49 -0800

    add prototype of board

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,7 +1,22 @@
-function TicTacToe() {
+var turn = 0;
+var icon = 'X';
+var player = 'Player 1';
 
+function TicTacToe() {
+  this.board = [[""], ["","",""], ["","",""]];
+//  "td"."cell 0"= board[0][0];
 }
 
 TicTacToe.prototype = {
-  
-}
+
+};
+
+$("td").bind("click", function(){
+  if (turn%2 === 0){
+    $(this).addClass("x-clicked");
+    turn = turn + 1;
+  }else{
+    $(this).addClass("o-clicked");
+    turn = turn + 1;
+  }
+});
