5b1db4da1a8b3dfec1e222fa4157054a81dc2a9e
Desiree <desireepoland@gmail.com>
2016-01-28 14:09:10 -0800

    Can add an x into each square

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,7 +1,33 @@
 function TicTacToe() {
-  this.board = [[0,0,0],[0,0,0],[0,0,0]];
+  this.board = [[0,0,0],
+                [0,0,0],
+                [0,0,0]];
 }
 
-TicTacToe.prototype = {
-
-}
+$("#r1c1").on("click", function() {
+    $("#r1c1").append("x");
+  });
+$("#r1c2").on("click", function() {
+    $("#r1c2").append("x");
+  });
+$("#r1c3").on("click", function() {
+    $("#r1c3").append("x");
+  });
+$("#r2c1").on("click", function() {
+    $("#r2c1").append("x");
+  });
+$("#r2c2").on("click", function() {
+    $("#r2c2").append("x");
+  });
+$("#r2c3").on("click", function() {
+    $("#r2c3").append("x");
+  });
+$("#r3c1").on("click", function() {
+    $("#r3c1").append("x");
+  });
+$("#r3c2").on("click", function() {
+    $("#r3c2").append("x");
+  });
+$("#r3c3").on("click", function() {
+    $("#r3c3").append("x");
+  });
