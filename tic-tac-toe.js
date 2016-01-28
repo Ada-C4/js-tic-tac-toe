8775c229a3bcc9f43e086476d7ea073ab931191d
Kelly <kelly_devlin@alumni.brown.edu>
2016-01-28 11:10:52 -0800

    initial play turn functionality

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -4,20 +4,27 @@ function TicTacToe() {
                 [0, 0, 0]
                 ];
   this.current_player = 1;
+  this.current_player_marker = "😜";
 }
 
 TicTacToe.prototype.play_turn = function(row, column) {
   this.board[row][column] = this.current_player;
   if (this.current_player === 1) {
     this.current_player = 2;
+    this.current_player_marker = "🙈";
   }
   else {
     this.current_player = 1;
+    this.current_player_marker = "😜";
   }
 };
 
 $(document).ready(function() {
+  var game = new TicTacToe();
 
-
+  $(".row_1.column_1").click(function(){
+    $(this).text(game.current_player_marker);
+    game.play_turn(0,0);
+  });
 
 });
