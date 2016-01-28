e00f00072d8addca93d11d8a0d3fa6d07ae7a194
Ricky Hougland <houglande@gmail.com>
2016-01-28 10:00:31 -0800

    set up divs for board game, begin connecting html to js

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,7 +1,11 @@
 function TicTacToe() {
-
+  this.boardTiles = [["row-0-col-0", "row-0-col-1", "row-0-col-2"], ["row-1-col-0", "row-1-col-1", "row-1-col-2"], [["row-2-col-0", "row-2-col-1", "row-2-col-2"]]];
 }
 
 TicTacToe.prototype = {
-  
-}
+  this.boardTiles.forEach( function(tile) {
+    $('.' + tile).click( function() {
+      console.log(title + " wow");
+    });
+  });
+};
