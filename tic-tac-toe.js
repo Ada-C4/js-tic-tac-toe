4f434b44dcf0761054a3b0b88e481d6ac39f2335
Katherine Defliese <kdefliese@users.noreply.github.com>
2016-01-28 10:18:46 -0800

    added basic game structure

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,7 +1,11 @@
 function TicTacToe() {
-
+  this.board = [[0,0,0],[0,0,0],[0,0,0]];
+  this.state = "new game";
+  this.turn_count = 0;
 }
 
 TicTacToe.prototype = {
-  
-}
+  turn: function() {
+    console.log("A turn has occurred");
+  }
+};
