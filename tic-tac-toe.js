11b1e09a5a07bb8323392721bc728137dede4694
emgord <emgord@gmail.com>
2016-01-29 11:57:28 -0800

    added function to check if game is over and declare the winner

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,13 +1,17 @@
 function TicTacToe() {
-  this.board = [[0,0,0],[0,0,0],[0,0,0]];
+  this.board = [[1,2,3],[4,5,6],[7,8,9]];
   this.playerOne = "X";
   this.playerTwo = "O";
   this.currentPlayer = this.playerOne;
+  this.gameOver = false;
 }
 
 TicTacToe.prototype.play = function(clicked){
-  this.turn(clicked);
-  this.togglePlayer();
+  if (this.gameOver === false) {
+    this.turn(clicked);
+    this.checkWinner();
+    this.togglePlayer();
+  }
 };
 
 
@@ -25,11 +29,40 @@ TicTacToe.prototype.turn = function(clicked) {
   }
 };
 
-
 TicTacToe.prototype.togglePlayer = function(){
   if (this.currentPlayer == this.playerOne) {
     this.currentPlayer = this.playerTwo;
   } else if (this.currentPlayer == this.playerTwo) {
     this.currentPlayer = this.playerOne;
+  }
+  // $("#alert").text(this.currentPlayer + "'s turn");
+};
+
+TicTacToe.prototype.checkWinner = function(){
+  //checking across
+  for (var i=0; i<this.board.length; i++) {
+    var arrayRow = this.board[i];
+    if (arrayRow[0] == arrayRow[1] && arrayRow[1] == arrayRow[2]){
+        $("#alert").text("Player " + this.currentPlayer + " is the winner!");
+        this.gameOver = true;
+      }
+    }
+  //checking vertically
+  var firstRow = this.board[0];
+  var secondRow = this.board[1];
+  var thirdRow = this.board[2];
+  for (var j=0; j<this.board.length; j++) {
+    if (firstRow[j] == secondRow[j] && secondRow[j] == thirdRow[j]){
+      $("#alert").text("Player " + this.currentPlayer + " is the winner!");
+      this.gameOver = true;
+    }
+  }
+  //checking diagonally
+  if (firstRow[0] == secondRow[1] && secondRow[1] == thirdRow[2]){
+    $("#alert").text("Player " + this.currentPlayer + " is the winner!");
+    this.gameOver = true;
+  } else if (firstRow[2] == secondRow[1] && secondRow[1] == thirdRow[0]){
+    $("#alert").text("Player " + this.currentPlayer + " is the winner!");
+    this.gameOver = true;
   }
 };
