feaa94fd040fd5878019eb793098c700af2f495c
Desiree <desireepoland@gmail.com>
2016-01-29 09:33:38 -0800

    toggle between players marks

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,18 +1,39 @@
 function TicTacToe() {
   this.board = [[0,0,0],[0,0,0],[0,0,0]];
-  this.player =
   this.gameOver = false;
+  this.play = 1;
+  this.player = 1;
+  this.clickSpace();
 }
 
-TicTacToe.prototype.checkGameOver =function() {
-  var winConditions = [[0,1,2], [3,4,5], [6,7,8],[0,3,6], [1,4,7], [2,5,8],[0,4,8], [6,4,2]];
+
+// TicTacToe.prototype.checkGameOver =function() {
+//   var winConditions = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8],[0,4,8], [6,4,2]];
+// };
+
+
+TicTacToe.prototype.drawMark =function(player) {
+  // var player = player || 1;
+  // if ( player == 1){
+  //   player = 2;
+  // } else {
+  //   player = 1;
+  // }
+  // console.log(player)
+  return player == 1 ? 'X' : 'O';
 };
 
-
-// DRY this code up
-// This adds stuff into each
-$(".square").on("click", function() {
-    if(this.innerText == ""){
-      $(this).append("x");
-    };
-  });
+TicTacToe.prototype.clickSpace =function() {
+  var self = this;
+  var player = player || 1;
+  $(".square").on("click", function() {
+      if(this.innerText == ""){
+        $(this).append(self.drawMark(player));
+        if ( player == 1){
+          player = 2;
+        } else {
+          player = 1;
+        }
+      };
+    });
+  };
