0c1e257197b941455476c043411558c3505a8365
Kelly <kelly_devlin@alumni.brown.edu>
2016-01-29 15:10:03 -0800

    Add background and refactor to make players objects

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -3,51 +3,56 @@ function TicTacToe() {
                 [0, 0, 0],
                 [0, 0, 0]
                 ];
-  this.current_player = 1;
-  this.current_player_marker = "😜";
+  this.player_1 = new Player(1, "Piggy", "🐷");
+  this.player_2 = new Player(2, "Munksy", "🐵");
+  this.current_player = this.player_2.isTurn ? this.player_2 : this.player_1;
   this.winning_sequence = null;
 }
 
+function Player(number, name, marker) {
+  this.number = number;
+  this.name = name;
+  this.marker = marker;
+  this.isTurn = false;
+}
+
 TicTacToe.prototype.playTurn = function(row, column) {
-  this.board[row][column] = this.current_player;
+  this.board[row][column] = this.current_player.number;
   this.checkWinningBoard();
   if (this.winning_sequence) {
       $( "td" ).empty();
-      $(".row_" + this.winning_sequence[0][0] + ".column_" + this.winning_sequence[0][1]).text(this.current_player_marker);
-      $(".row_" + this.winning_sequence[1][0] + ".column_" + this.winning_sequence[1][1]).text(this.current_player_marker);
-      $(".row_" + this.winning_sequence[2][0] + ".column_" + this.winning_sequence[2][1]).text(this.current_player_marker);
+      $(".row_" + this.winning_sequence[0][0] + ".column_" + this.winning_sequence[0][1]).text(this.current_player.marker);
+      $(".row_" + this.winning_sequence[1][0] + ".column_" + this.winning_sequence[1][1]).text(this.current_player.marker);
+      $(".row_" + this.winning_sequence[2][0] + ".column_" + this.winning_sequence[2][1]).text(this.current_player.marker);
   } else {
     this.switch_player();
   }
 };
 
 TicTacToe.prototype.switch_player = function() {
-  if (this.current_player === 1) {
-    this.current_player = 2;
-    this.current_player_marker = "🙈";
-  }
-  else {
-    this.current_player = 1;
-    this.current_player_marker = "😜";
+  if (this.current_player == this.player_1) {
+    this.current_player = this.player_2;
+  } else {
+    this.current_player = this.player_1;
   }
 };
 
 TicTacToe.prototype.checkWinningBoard = function() {
-  if ((this.board[0][0] === this.current_player) && (this.board[0][1] === this.current_player) && (this.board[0][2] === this.current_player)) {
+  if ((this.board[0][0] === this.current_player.number) && (this.board[0][1] === this.current_player.number) && (this.board[0][2] === this.current_player.number)) {
     this.winning_sequence = [[0, 0], [0, 1], [0, 2]];
-  } else if ((this.board[1][0] === this.current_player) && (this.board[1][1] === this.current_player) && (this.board[1][2] === this.current_player)) {
+  } else if ((this.board[1][0] === this.current_player.player) && (this.board[1][1] === this.current_player.number) && (this.board[1][2] === this.current_player.number)) {
     this.winning_sequence = [[1,0], [1,1], [1,2]];
-  } else if ((this.board[2][0] === this.current_player) && (this.board[2][1] === this.current_player) && (this.board[2][2] === this.current_player)) {
+  } else if ((this.board[2][0] === this.current_player.number) && (this.board[2][1] === this.current_player.number) && (this.board[2][2] === this.current_player.number)) {
     this.winning_sequence = [[2,0], [2,1], [2,2]];
-  } else if ((this.board[0][0] === this.current_player) && (this.board[1][0] === this.current_player) && (this.board[2][0] === this.current_player)) {
+  } else if ((this.board[0][0] === this.current_player.number) && (this.board[1][0] === this.current_player.number) && (this.board[2][0] === this.current_player.number)) {
     this.winning_sequence = [[0,0], [1,0], [2,0]];
-  } else if ((this.board[0][1] === this.current_player) && (this.board[1][1] === this.current_player) && (this.board[2][1] === this.current_player)) {
+  } else if ((this.board[0][1] === this.current_player.number) && (this.board[1][1] === this.current_player.number) && (this.board[2][1] === this.current_player.number)) {
     this.winning_sequence = [[0,1], [1,1], [2,1]];
-  } else if ((this.board[0][2] === this.current_player) && (this.board[1][2] === this.current_player) && (this.board[2][2] === this.current_player)) {
+  } else if ((this.board[0][2] === this.current_player.number) && (this.board[1][2] === this.current_player.number) && (this.board[2][2] === this.current_player.number)) {
     this.winning_sequence = [[0,2], [1,2], [2,2]];
-  } else if ((this.board[0][0] === this.current_player) && (this.board[1][1] === this.current_player) && (this.board[2][2] === this.current_player)) {
+  } else if ((this.board[0][0] === this.current_player.number) && (this.board[1][1] === this.current_player.number) && (this.board[2][2] === this.current_player.number)) {
     this.winning_sequence = [[0,0], [1,1], [2,2]];
-  } else if ((this.board[0][2] === this.current_player) && (this.board[1][1] === this.current_player) && (this.board[2][0] === this.current_player)) {
+  } else if ((this.board[0][2] === this.current_player.number) && (this.board[1][1] === this.current_player.number) && (this.board[2][0] === this.current_player.number)) {
     this.winning_sequence = [[0,2], [1,1], [2,0]];
   }
 };
@@ -60,7 +65,7 @@ $(document).ready(function() {
       if (game.board[row][column] !== 0) {
         throw new Error ("This spot is already claimed!");
       }
-      $(this).text(game.current_player_marker);
+      $(this).text(game.current_player.marker);
       game.playTurn(row,column);
     });
   };
