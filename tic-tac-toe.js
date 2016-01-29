0d5e254a05877b4df43b82476b7b54eb402e0971
Jenna Nichols <jennaplusplus@gmail.com>
2016-01-29 11:11:24 -0800

    some win logic

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -3,17 +3,85 @@ function TicTacToe() {
   this.playerOne = 'X';
   this.playerTwo = 'O';
   this.currentPlayer = this.playerOne;
+  this.turns = 0;
 }
 
-TicTacToe.prototype.play = function() {
-  this.takeTurn(this.currentPlayer);
+TicTacToe.prototype.updateBoard = function(square) {
+  switch ($(square).attr('id')) {
+    case "top-left":
+      this.board[0][0] = this.currentPlayer;
+      break;
+    case "top-mid":
+      this.board[0][1] = this.currentPlayer;
+      break;
+    case "top-right":
+      this.board[0][2] = this.currentPlayer;
+      break;
+    case "mid-left":
+      this.board[1][0] = this.currentPlayer;
+      break;
+    case "mid-mid":
+      this.board[1][1] = this.currentPlayer;
+      break;
+    case "mid-right":
+      this.board[1][2] = this.currentPlayer;
+      break;
+    case "bottom-left":
+      this.board[2][0] = this.currentPlayer;
+      break;
+    case "bottom-mid":
+      this.board[2][1] = this.currentPlayer;
+      break;
+    case "bottom-right":
+      this.board[2][2] = this.currentPlayer;
+      break;
+  }
 };
 
-TicTacToe.prototype.takeTurn = function(player) {
-  // update board attributes
-  // figure out if someone won
+TicTacToe.prototype.play = function() {
+  if (this.gameOver() === false) {
+    this.switchPlayer();
+  } else {
+    $("body").append("<h2>Game Over 😶</h2>");
+  }
+};
 
-  this.switchPlayer();
+TicTacToe.prototype.gameOver = function() {
+  var b = this.board;
+  var one = this.playerOne;
+  var two = this.playerTwo;
+
+  // variables for the board
+  var topLeft = b[0][0], topMid = b[0][1], topRight = b[0][2]; // top
+  var midLeft = b[1][0], midMid = b[1][1], midRight = b[1][2]; // middle
+  var bottomLeft = b[2][0], bottomMid = b[2][1], bottomRight = b[2][2]; // bottom
+
+
+  // player one wins vertically
+  if (topLeft === one && midLeft === one && bottomLeft === one) {
+    return true;
+  } else if (topMid === one && midMid === one && bottomMid === one) {
+    return true;
+  } else if (topRight === one && midRight === one && bottomRight === one) {
+    return true;
+  }
+
+  // player two wins vertically
+  if (topLeft === two && midLeft === two && bottomLeft === two) {
+    return true;
+  } else if (topMid === two && midMid === two && bottomMid === two) {
+    return true;
+  } else if (topRight === two && midRight === two && bottomRight === two) {
+    return true;
+  }
+
+  return false;
+
+  // if (this.turns > 8) {
+  //   return true;
+  // } else {
+  //   return false;
+  // }
 };
 
 TicTacToe.prototype.switchPlayer = function () {
