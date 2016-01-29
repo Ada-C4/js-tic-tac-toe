94fb804d91ab9f6c4f2c6ca987dc13a51111162c
Meighan Rasley <meighanr@gmail.com>
2016-01-28 21:05:21 -0800

    Can only click on a cell one time

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -3,19 +3,19 @@ $(document).ready(function () {
   console.log('create and begin the game here!');
   var cells = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   cells.forEach(function(cell) {
-    $("#cell" + cell).click(function () {
+    $("#cell" + cell).one( "click", (function () {
       $(this).css("background-color", "#16A765");
       if (game.turns % 2 === 0) {
         $(this).css("background-color", "#000");
+        this.player = "player2";
       }
       game.turn();
-    });
+    }));
   });
 });
 
 function TicTacToe() {
-  this.player1 = "X";
-  this.player2 = "O";
+  this.player = "player1";
   this.turns = 1;
 }
 
