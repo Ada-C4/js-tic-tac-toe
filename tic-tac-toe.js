dca68c64a8e2265289cc4297b8b63436b86d9fd4
Desiree <desireepoland@gmail.com>
2016-01-29 10:28:37 -0800

    refactor code

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -7,44 +7,42 @@ function TicTacToe() {
 //   var winConditions = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8],[0,4,8], [6,4,2]];
 // };
 
-// merge these later like this
-//TicTacToe.prototype ={
-//  drawMark: function(){
-//  },
-// };
-
 // reset board
 
-TicTacToe.prototype.drawMark =function(player) {
-  return player == 1 ? '🍍' : '🌺';
-};
+
+TicTacToe.prototype = {
+ drawMark: function(player){
+   return player == 1 ? '🍍' : '🌺';
+ },
 
 //check every square - if they are all filled, then do alert/game over TicTacToe
-TicTacToe.prototype.checkForTie =function() {
-  var filled = 0;
-  $(".square").each(function(){
-    debugger;
-    if(this.innerText == ""){
-      filled++;
-    }
-  });
-  if (filled == 0){
-    alert("Game Over - It's a tie!");
-  }
-};
+ checkForTie: function() {
+   var filled = 0;
+   $(".square").each(function(){
+     debugger;
+     if(this.innerText == ""){
+       filled++;
+     }
+   });
+   if (filled == 0){
+     alert("Game Over - It's a tie!");
+   }
+ },
 
-TicTacToe.prototype.clickSpace =function() {
-  var self = this;
-  var player = player || 1;
-  $(".square").on("click", function() {
-      if(this.innerText == ""){
-        $(this).append(self.drawMark(player));
-        if ( player == 1){
-          player = 2;
-        } else {
-          player = 1;
-        }
-      };
-      self.checkForTie();
-    });
-  };
+ clickSpace: function() {
+   var self = this;
+   var player = player || 1;
+   $(".square").on("click", function() {
+       if(this.innerText == ""){
+         $(this).append(self.drawMark(player));
+         if ( player == 1){
+           player = 2;
+         } else {
+           player = 1;
+         }
+       };
+       self.checkForTie();
+     });
+   }
+
+};
