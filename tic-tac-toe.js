b2decb7f8814ff4530c371ae78cfe9557d21b211
Sarah <trowbrsa@gmail.com>
2016-01-29 14:24:32 -0800

    Updated board so that it now includes the position of the play for each player on each turn

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,6 +1,7 @@
 $(document).on('ready', function() {
   var game = new TicTacToe();
 
+// we could have saved the function to a variable and then passed that variable to click
   $(":button").click( function(){
     if (game.turns % 2 === 0) {
       $(this).css('background-color', 'red');
@@ -9,7 +10,7 @@ $(document).on('ready', function() {
     }
     // this gives me the class that the person has clicked on and is storing the value so I can use it elsewhere
     var position = $(this).attr("class").split(" ")[1];
-    game.play(position, game.marker);
+    game.play(position, game.marker());
   });
 });
 
@@ -25,11 +26,12 @@ $(document).on('ready', function() {
   TicTacToe.prototype.play = function(position, marker) {
   // this is where all my logic will live
   // logic for when someone clicks on a color, slots are updated
-  // how do I refer to empty board?
   // var slots = ['0', '1', '2', '3', '4', '5', '6', '7', '8'];
   // this calls the turn method below. without () it would not run as a function
   this.turn();
-  this.board.push(position);
+  // now, push the x or y into the particular index we want. may eventually want a double loop.
+  // this.board.push(position);
+  this.board[position] = marker;
   // take the index of the slots array and use that to say where to push it on board.
     // slots.forEach( function(slot) {
     //     // update array: board will now have an x for the particular slot its going in
@@ -54,6 +56,9 @@ $(document).on('ready', function() {
     }
   };
 
+  TicTacToe.prototype.marker = function(board){
+    
+  };
 
 // var won = function(board){
 //   // loop through and see if someone has won horizontally, vertically, diagonally
