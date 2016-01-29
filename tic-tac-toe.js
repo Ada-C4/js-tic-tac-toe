969864cb90df949615cefa46bc1f1ba469d3a8a9
Claire <ClaireS29@gmail.com>
2016-01-29 12:03:15 -0800

    updated board logic to an array of arrays with working winning loop conditionals

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,5 +1,7 @@
 function TicTacToe() {
-  this.addMarker();
+  this.currentPlayer = 0;
+  this.board = [["r1c1", "r1c2", "r1c3"], ["r2c1", "r2c2", "r2c3"], ["r3c1", "r3c2", "r3c3"]];
+  this.play();
 }
 
 // TicTacToe.prototype.play = function() {
@@ -7,37 +9,62 @@ function TicTacToe() {
 // };
 
 TicTacToe.prototype = {
-  currentPlayer: 0,
-  board: ["r1c1", "r1c2", "r1c3", "r2c1", "r2c2", "r2c3", "r3c1", "r3c2", "r3c3"],
-  changePlayer: function () {
-       this.currentPlayer = this.currentPlayer ? 0 : 1;
-     },
+  play: function() {
+    this.addMarker();
+  },
 
   addMarker: function() {
-    this.board.forEach(function(cell) {
-      $("#" + cell).click(function(){
-        if (this.currentPlayer === 0) {
-        $("#" + cell).append('X');
-        changePlayer();
-      } else {
-        $("#" + cell).append('O');
-        changePlayer();
-      }
+    var self = this;
+    self.board.forEach(function(row, rowIndex) {
+      row.forEach(function(cell, cellIndex) {
+        $("#" + cell).one ("click", function(){
+          $("#" + cell).text(self.getMarker());
+        self.board[rowIndex][cellIndex] = self.getMarker();
+        self.checkforWin();
+        self.changePlayer();
+        });
       });
     });
   },
 
-  play: function() {
-    addMarker();
-  }
+  getMarker: function() {
+    if (this.currentPlayer === 1){
+       return 'X';
+    } else {
+       return 'O';
+    }
+  },
+
+  changePlayer: function () {
+    this.currentPlayer = this.currentPlayer ? 0 : 1;
+  },
+
+  checkforWin: function () {
+    var self = this; won = false;
+
+    self.board.forEach(function(row) {
+      if (row[0] == row[1] && row[0] == row[2]) {
+        won = true;
+        console.log(won);
+      }
+    });
+
+    self.board[0].forEach(function(cell, cellIndex) {
+      if (self.board[0][cellIndex] == self.board[1][cellIndex] && self.board[0][cellIndex] == self.board[2][cellIndex]) {
+        won = true;
+        console.log(won);
+      }
+    });
+
+    if (self.board[0][0] == self.board[1][1] && self.board[0][0] == self.board[2][2]) {
+      won = true;
+      console.log(won);
+
+    }
+    if (self.board[0][2] == self.board[1][1] && self.baord[0][2] == self.board[2][0]) {
+      won = true;
+      console.log(won);
+    }
+  },
+
 };
-
-// function addMarker(playerMarker){
-//   $("#r1c1").click(function(){
-//     $("#r1c1").append(playerMarker);
-//   });
-// }
-
-// setSymbol = function () {
-//   var currentSymbol = currentPlayer ? 'X' : 'O';
-// };
