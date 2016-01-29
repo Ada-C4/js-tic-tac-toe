611cfab5f7159d2244cbd772e0674afdfb81e7c1
Katherine Defliese <kdefliese@users.noreply.github.com>
2016-01-29 11:15:27 -0800

    winner displayed after each game

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -17,7 +17,10 @@ TicTacToe.prototype.turn = function(cell_id) {
   else {
     this.markCell(cell_id, this.whose_turn);
     this.incrementTurn();
-    this.checkWin();
+    var winner = this.checkWin()[1];
+    if (this.state == "game over") {
+      this.displayWinner(winner);
+    }
   }
 };
 
@@ -59,37 +62,58 @@ TicTacToe.prototype.incrementTurn = function() {
 };
 
 TicTacToe.prototype.checkWin = function() {
+  var winner;
   if (this.board[0] == this.board[1] && this.board[1] == this.board[2]) {
     this.state = "game over";
+    winner = this.board[0];
   }
   else if (this.board[3] == this.board[4] && this.board[4] == this.board[5]) {
     this.state = "game over";
+    winner = this.board[3];
   }
   else if (this.board[6] == this.board[7] && this.board[7] == this.board[8]) {
     this.state = "game over";
+    winner = this.board[6];
   }
   else if (this.board[0] == this.board[3] && this.board[3] == this.board[6]) {
     this.state = "game over";
+    winner = this.board[0];
   }
   else if (this.board[1] == this.board[4] && this.board[4] == this.board[7]) {
     this.state = "game over";
+    winner = this.board[1];
   }
   else if (this.board[2] == this.board[5] && this.board[5] == this.board[8]) {
     this.state = "game over";
+    winner = this.board[2];
   }
   else if (this.board[2] == this.board[4] && this.board[4] == this.board[6]) {
     this.state = "game over";
+    winner = this.board[2];
   }
   else if (this.board[0] == this.board[4] && this.board[4] == this.board[8]) {
     this.state = "game over";
+    winner = this.board[0];
   }
-  return this.state;
+  var return_vals = [this.state, winner];
+  return return_vals;
 };
 
-TicTacToe.prototype.clearBoard = function() {
+TicTacToe.prototype.displayWinner = function(winner) {
+  if (winner == "X") {
+    $("#player_one").append("Player One wins!");
+  }
+  else {
+    $("#player_two").append("Player Two wins!");
+  }
+};
+
+TicTacToe.prototype.newGame = function() {
   game = new TicTacToe();
   for (var i = 0; i < 9; i++) {
     $("#" + i.toString()).html("");
   }
+  $("#player_one").html("");
+  $("#player_two").html("");
   return game;
 };
