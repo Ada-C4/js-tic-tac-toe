b70af4036119793f411c9319a35d508ce5925b13
Sarah <trowbrsa@gmail.com>
2016-01-29 12:26:48 -0800

    Added logic for assigning x and y value for player 1 and player 2

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -7,11 +7,9 @@ $(document).on('ready', function() {
     } else {
       $(this).css('background-color', 'green');
     }
-    var colorPlayed = $("slot").css('background-color');
     // this gives me the class that the person has clicked on and is storing the value so I can use it elsewhere
     var position = $(this).attr("class");
     game.play(position);
-    game.play(colorPlayed);
   });
 });
 
@@ -21,20 +19,17 @@ $(document).on('ready', function() {
     this.board = [];
     this.player = "player1";
     this.turns = 0;
-    /// to delete
-    this.colors = [];
     // this.won = nil;
   }
 
-  TicTacToe.prototype.play = function(position, colorPlayed) {
+  TicTacToe.prototype.play = function(position, marker) {
   // this is where all my logic will live
   // logic for when someone clicks on a color, slots are updated
   // how do I refer to empty board?
-  var slots = ['0', '1', '2', '3', '4', '5', '6', '7', '8'];
+  // var slots = ['0', '1', '2', '3', '4', '5', '6', '7', '8'];
   // this calls the turn method below. without () it would not run as a function
   this.turn();
   this.board.push(position);
-  this.colors.push(colorPlayed);
   // take the index of the slots array and use that to say where to push it on board.
     // slots.forEach( function(slot) {
     //     // update array: board will now have an x for the particular slot its going in
@@ -50,6 +45,15 @@ $(document).on('ready', function() {
       this.player = "player2";
     }
   };
+
+  TicTacToe.prototype.marker = function(player){
+    if (this.player = "player1") {
+      var 'x' = "X";
+    } else {
+      var 'y' = "Y";
+    }
+  };
+
 
 // var won = function(board){
 //   // loop through and see if someone has won horizontally, vertically, diagonally
