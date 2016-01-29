c10e891e4e7cffd795184acf60e986ffda62c6ad
Wesley Willis <haileyw@gmail.com>
2016-01-29 09:33:11 -0800

    add method to match clicked td to boardState array

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,22 +1,42 @@
 var turn = 0;
 var icon = 'X';
 var player = 'Player 1';
+var boardState = new TicTacToe();
 
 function TicTacToe() {
-  this.board = [[""], ["","",""], ["","",""]];
-//  "td"."cell 0"= board[0][0];
+  this.board = ["cell 0", "cell 1","cell 2","cell 3","cell 4","cell 5","cell 6","cell 7","cell 8"];
 }
 
-TicTacToe.prototype = {
 
+
+TicTacToe.prototype.getBoard = function() {
+  return this.board;
 };
 
-$("td").bind("click", function(){
-  if (turn%2 === 0){
-    $(this).addClass("x-clicked");
-    turn = turn + 1;
-  }else{
-    $(this).addClass("o-clicked");
-    turn = turn + 1;
-  }
+
+$("td").bind("click", function() {
+
+    if (turn%2 === 0){
+     //board.forEach(box);{
+       //if(this === box){
+        // box = "X";
+       //}
+       var index = 0;
+       for (index = 0; index < boardState.board.length; index ++){
+         if (boardState.board[index] === this.className){
+           alert(boardState.board[index] + "  =  " + this.className + "  YAY");
+           $(this).addClass("x-clicked");
+         }
+       }
+     }
+
+       //alert(this.className);}
+
+
+    // $(this).addClass("x-clicked");
+      turn = turn + 1;
+  // }else{
+    // $(this).addClass("o-clicked");
+    // turn = turn + 1;
+  // }
 });
