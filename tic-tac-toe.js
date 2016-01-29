dd8312fc3b527896fd1d92c58d3d27a1b087256f
Rebecca Tolmach <rmtolmach@gmail.com>
2016-01-29 10:37:16 -0800

    can click on all squares

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,10 +1,26 @@
 var TicTacToe = function() {
  // initialize things when a new game starts
+ this.board = ["one", "two", "three", "four", "five", "six", "seven"];
 };
 
 
   TicTacToe.prototype.play = function() {
-    $(".one").click(function() {
-      $(this).html("<h1>X</h1>");
-    });
+//when a player
+//how to update the array??
+// how to access board in here.
+//player 1 will be 1s and player 2 will be 2s.
+    var self = this;
+      self.board.forEach(function(cell) {
+        $("#" + cell).click(function() {
+          $("#" + cell).html(///call function that returns X or 0 changes player and
+          );
+
+        });
+      });
+
+
+
+
   };
+
+// player 1 and player 2 that switch back and forth
