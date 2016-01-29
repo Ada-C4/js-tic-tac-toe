11fbe448fd9cdd1f7cd24c81022d9dd9fd302b12
Kelly <kelly_devlin@alumni.brown.edu>
2016-01-29 15:41:04 -0800

    Update CSS and remove unnecessary isTurn attribute

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -3,9 +3,9 @@ function TicTacToe() {
                 [0, 0, 0],
                 [0, 0, 0]
                 ];
-  this.player_1 = new Player(1, "Piggy", "🐷");
-  this.player_2 = new Player(2, "Munksy", "🐵");
-  this.current_player = this.player_2.isTurn ? this.player_2 : this.player_1;
+  this.player_1 = new Player(1, "Ham", "🐷");
+  this.player_2 = new Player(2, "Abu", "🐵");
+  this.current_player = this.player_1;
   this.winning_sequence = null;
 }
 
@@ -13,7 +13,6 @@ function Player(number, name, marker) {
   this.number = number;
   this.name = name;
   this.marker = marker;
-  this.isTurn = false;
 }
 
 TicTacToe.prototype.playTurn = function(row, column) {
@@ -24,13 +23,14 @@ TicTacToe.prototype.playTurn = function(row, column) {
       $(".row_" + this.winning_sequence[0][0] + ".column_" + this.winning_sequence[0][1]).text(this.current_player.marker);
       $(".row_" + this.winning_sequence[1][0] + ".column_" + this.winning_sequence[1][1]).text(this.current_player.marker);
       $(".row_" + this.winning_sequence[2][0] + ".column_" + this.winning_sequence[2][1]).text(this.current_player.marker);
+      $( "table").after("<h1>" + this.current_player.name + " Wins!" + "</h1>");
   } else {
     this.switch_player();
   }
 };
 
 TicTacToe.prototype.switch_player = function() {
-  if (this.current_player == this.player_1) {
+  if (this.current_player === this.player_1) {
     this.current_player = this.player_2;
   } else {
     this.current_player = this.player_1;
@@ -40,7 +40,7 @@ TicTacToe.prototype.switch_player = function() {
 TicTacToe.prototype.checkWinningBoard = function() {
   if ((this.board[0][0] === this.current_player.number) && (this.board[0][1] === this.current_player.number) && (this.board[0][2] === this.current_player.number)) {
     this.winning_sequence = [[0, 0], [0, 1], [0, 2]];
-  } else if ((this.board[1][0] === this.current_player.player) && (this.board[1][1] === this.current_player.number) && (this.board[1][2] === this.current_player.number)) {
+  } else if ((this.board[1][0] === this.current_player.number) && (this.board[1][1] === this.current_player.number) && (this.board[1][2] === this.current_player.number)) {
     this.winning_sequence = [[1,0], [1,1], [1,2]];
   } else if ((this.board[2][0] === this.current_player.number) && (this.board[2][1] === this.current_player.number) && (this.board[2][2] === this.current_player.number)) {
     this.winning_sequence = [[2,0], [2,1], [2,2]];
