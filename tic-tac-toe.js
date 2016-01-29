6dfcc63df182240fbb7b516aa39dae29fad5b106
Wesley Willis <haileyw@gmail.com>
2016-01-29 14:46:03 -0800

    seperated logic into functions

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,8 +1,6 @@
 var turn = 0;
 var icon = 'X';
 var player = 'Player 1';
-var boardState = new TicTacToe();
-//var playGame = boardState.markBoard;
 
 function TicTacToe() {
   this.board = ["cell 0", "cell 1","cell 2","cell 3","cell 4","cell 5","cell 6","cell 7","cell 8"];
@@ -14,45 +12,59 @@ TicTacToe.prototype.getBoard = function() {
   return this.board;
 };
 
-//TicTacToe.prototype.markBoard = function() {
-$("td").bind("click", function() {
-  if (turn%2 === 0){
+TicTacToe.prototype.markBoard = function() {
+  var self = this;
+  $("td").bind("click", function() {
     var index = 0;
-      for (index = 0; index < boardState.board.length; index ++){
-        if (boardState.board[index] === this.className){
-        //  alert(boardState.board[index] + "  =  " + this.className + "  YAY");
-          $(this).addClass("x-clicked");
-          boardState.board[index] = "x";
-          turn = turn + 1;
+    if (turn%2 === 0){
+      //var index = 0;
+        for (index = 0; index < self.board.length; index ++){
+          if (self.board[index] === this.className){
+            $(this).addClass("x-clicked");
+            self.board[index] = "X";
+            turn = turn + 1;
+          }
+        }
+      }else{
+      //  index = 0;
+        for (index = 0; index < self.board.length; index ++){
+          if (self.board[index] === this.className){
+            //alert(this.board[index] + "  =  " + this.className + "  YAY");
+            $(this).addClass("o-clicked");
+            self.board[index] = "O";
+            turn = turn + 1;
+          }
         }
       }
-  }else{
-    var index = 0;
-    for (index = 0; index < boardState.board.length; index ++){
-      if (boardState.board[index] === this.className){
-        //alert(boardState.board[index] + "  =  " + this.className + "  YAY");
-        $(this).addClass("o-clicked");
-        boardState.board[index] = "o";
-        turn = turn + 1;
-      }
-    }
+      self.scoreBoard();
+    });
+  //this.scoreBoard();
+};
+
+TicTacToe.prototype.scoreBoard = function() {
+  if (this.board[1]===this.board[2]&&this.board[0]===this.board[1]){
+    alert("Player " + this.board[0] + " + has won!");
+    location.reload();
+  }else if(this.board[3]===this.board[4]&&this.board[5]===this.board[3]){
+    alert("Player " + this.board[3] + " + has won!");
+    location.reload();
+  }else if(this.board[6]===this.board[7]&&this.board[8]===this.board[6]){
+    alert("Player " + this.board[6] + " + has won!");
+    location.reload();
+  }else if(this.board[0]===this.board[3]&&this.board[0]===this.board[6]){
+    alert("Player " + this.board[6] + " + has won!");
+    location.reload();
+  }else if(this.board[1]===this.board[4]&&this.board[7]===this.board[1]){
+    alert("Player " + this.board[1] + " + has won!");
+    location.reload();
+  }else if(this.board[2]===this.board[5]&&this.board[8]===this.board[2]){
+    alert("Player " + this.board[2] + " + has won!");
+    location.reload();
+  }else if(this.board[2]===this.board[4]&&this.board[6]===this.board[2]){
+    alert("Player " + this.board[2] + " + has won!");
+    location.reload();
+  }else if(this.board[0]===this.board[4]&&this.board[8]===this.board[0]){
+    alert("Player " + this.board[0] + " + has won!");
+    location.reload();
   }
-  if (boardState.board[1]===boardState.board[2]&&boardState.board[0]===boardState.board[1]){
-    alert("Player " + boardState.board[0] + " + has won!");
-  }else if(boardState.board[3]===boardState.board[4]&&boardState.board[5]===boardState.board[3]){
-    alert("Player " + boardState.board[3] + " + has won!");
-  }else if(boardState.board[6]===boardState.board[7]&&boardState.board[8]===boardState.board[6]){
-    alert("Player " + boardState.board[6] + " + has won!");
-  }else if(boardState.board[0]===boardState.board[3]&&boardState.board[0]===boardState.board[6]){
-    alert("Player " + boardState.board[6] + " + has won!");
-  }else if(boardState.board[1]===boardState.board[4]&&boardState.board[7]===boardState.board[1]){
-    alert("Player " + boardState.board[1] + " + has won!");
-  }else if(boardState.board[2]===boardState.board[5]&&boardState.board[8]===boardState.board[2]){
-    alert("Player " + boardState.board[2] + " + has won!");
-  }else if(boardState.board[2]===boardState.board[4]&&boardState.board[6]===boardState.board[2]){
-    alert("Player " + boardState.board[2] + " + has won!");
-  }else if(boardState.board[0]===boardState.board[4]&&boardState.board[8]===boardState.board[0]){
-    alert("Player " + boardState.board[0] + " + has won!");
-  }
-});
-//};
+};
