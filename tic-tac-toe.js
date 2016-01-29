61564ca602b07083f5a844ecfd184cfaefa63cbc
emgord <emgord@gmail.com>
2016-01-29 09:26:45 -0800

    method now updates view and game array when player clicks element on screem

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,22 +1,30 @@
 function TicTacToe() {
   this.board = [[0,0,0],[0,0,0],[0,0,0]];
+  this.playerOne = "X";
+  this.playerTwo = "O";
+  this.currentPlayer = this.playerOne;
 }
 
-TicTacToe.prototype.drawBoard = function() {
+TicTacToe.prototype.play = function(clicked){
+  this.turn(clicked);
+};
+
+
+TicTacToe.prototype.turn = function(clicked) {
   var row = $(".row");
   for (var i=0; i < this.board.length; i++) {
     var squares = $(row[i]).children();
     var content = this.board[i];
     for (var i2=0; i2 < content.length; i2++) {
-      if (content[i2] === "X") {
-        $(squares[i2]).append("<p>X</p>");
-      } else if (content[i2] === "O") {
-        $(squares[i2]).append("<p>O</p>");
+      if(clicked.attr("id") == $(squares[i2]).attr("id")){
+        $(squares[i2]).append("<p>"+ this.currentPlayer +"</p>");
+        content[i2] = this.currentPlayer;
       }
     }
   }
 };
 
-TicTacToe.prototype.playTurn = function(){
-  
-}
+
+TicTacToe.prototype.togglePlayer = function(){
+
+};
