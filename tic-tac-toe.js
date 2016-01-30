891de200d46706bc996ecdceb0ba1511d2569973
emgord <emgord@gmail.com>
2016-01-29 16:04:55 -0800

    game takes user input to save player names

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,7 +1,9 @@
-function TicTacToe() {
+function TicTacToe(player1, player2) {
   this.board = [[1,2,3],[4,5,6],[7,8,9]];
-  this.playerOne = "X";
-  this.playerTwo = "O";
+  this.playerOne = { key: "X",
+                    name: player1};
+  this.playerTwo = { key: "O",
+                    name: player2};
   this.currentPlayer = this.playerOne;
   this.gameOver = false;
   this.turns = 0;
@@ -15,7 +17,7 @@ TicTacToe.prototype.play = function(clicked){
     if (this.turns === 9){
       $("#alert").text("Nobody Wins");
     } else if (this.gameOver === true){
-      $("#alert").text("Player " + this.currentPlayer + " is the winner!");
+      $("#alert").text(this.currentPlayer.name + " is the winner!");
     } else {
       this.togglePlayer();
     }
@@ -29,8 +31,8 @@ TicTacToe.prototype.turn = function(clicked) {
     var content = this.board[i];
     for (var i2=0; i2 < content.length; i2++) {
       if(clicked.attr("id") == $(squares[i2]).attr("id")){
-        $(squares[i2]).append("<p>"+ this.currentPlayer +"</p>");
-        content[i2] = this.currentPlayer;
+        $(squares[i2]).append("<p>"+ this.currentPlayer.key +"</p>");
+        content[i2] = this.currentPlayer.key;
       }
     }
   }
@@ -43,7 +45,7 @@ TicTacToe.prototype.togglePlayer = function(){
   } else if (this.currentPlayer == this.playerTwo) {
     this.currentPlayer = this.playerOne;
   }
-  $("#alert").text(this.currentPlayer + "'s turn");
+  $("#alert").text(this.currentPlayer.name + "'s turn");
 };
 
 TicTacToe.prototype.checkWinner = function(){
