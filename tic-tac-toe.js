1f5b7b0db1b1a523c7d76925e28b1b745fcde3fe
Claire <ClaireS29@gmail.com>
2016-01-30 16:55:07 -0800

    game finished and working

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,12 +1,8 @@
 function TicTacToe() {
-  this.currentPlayer = 0;
+  this.currentPlayer = "Toad";
   this.board = [["r1c1", "r1c2", "r1c3"], ["r2c1", "r2c2", "r2c3"], ["r3c1", "r3c2", "r3c3"]];
   this.play();
 }
-
-// TicTacToe.prototype.play = function() {
-//   addMarker();
-// };
 
 TicTacToe.prototype = {
   play: function() {
@@ -18,9 +14,11 @@ TicTacToe.prototype = {
     self.board.forEach(function(row, rowIndex) {
       row.forEach(function(cell, cellIndex) {
         $("#" + cell).one ("click", function(){
-          $("#" + cell).text(self.getMarker());
+          $("#" + cell).html('<img src='+self.getMarker()+'>');
         self.board[rowIndex][cellIndex] = self.getMarker();
-        self.checkforWin();
+        if (self.checkforWin()) {
+          $("#winners").text(self.currentPlayer + " has won!");
+        }
         self.changePlayer();
         });
       });
@@ -28,15 +26,15 @@ TicTacToe.prototype = {
   },
 
   getMarker: function() {
-    if (this.currentPlayer === 1){
-       return 'X';
+    if (this.currentPlayer === "Toad"){
+       return './frog.png';
     } else {
-       return 'O';
+       return './fly.png';
     }
   },
 
   changePlayer: function () {
-    this.currentPlayer = this.currentPlayer ? 0 : 1;
+    this.currentPlayer = (this.currentPlayer === "Fly") ? "Toad" : "Fly";
   },
 
   checkforWin: function () {
@@ -45,26 +43,21 @@ TicTacToe.prototype = {
     self.board.forEach(function(row) {
       if (row[0] == row[1] && row[0] == row[2]) {
         won = true;
-        console.log(won);
       }
     });
 
     self.board[0].forEach(function(cell, cellIndex) {
       if (self.board[0][cellIndex] == self.board[1][cellIndex] && self.board[0][cellIndex] == self.board[2][cellIndex]) {
         won = true;
-        console.log(won);
       }
     });
 
     if (self.board[0][0] == self.board[1][1] && self.board[0][0] == self.board[2][2]) {
       won = true;
-      console.log(won);
-
     }
-    if (self.board[0][2] == self.board[1][1] && self.baord[0][2] == self.board[2][0]) {
+    if (self.board[0][2] == self.board[1][1] && self.board[0][2] == self.board[2][0]) {
       won = true;
-      console.log(won);
     }
+    return won;
   },
-
 };
