e2635be8e20332426c7ecdad5e3b2fc3eff381e8
Meighan Rasley <meighanr@gmail.com>
2016-01-31 19:59:25 -0800

    Trying to get win logic to work

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -10,66 +10,82 @@ $(document).ready(function () {
       if (game.turns % 2 === 0) {
         $(this).css("background-color", "#000");
       }
-      game.turn(id);
+      game.play(id);
     }));
   });
 });
 
 function TicTacToe() {
-  this.player = "player1";
+  this.player = "Player 1";
   this.turns = 1;
-  this.board = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
+  this.board = [[0, 1, 2], [3, 4, 5], [6, 7, 8]];
   this.finish = false;
 }
 
 TicTacToe.prototype.turn = function(id) {
   var block = id[4] - 1;
-  var index;
+  var row;
   if (block < 3) {
-    index = 0;
+    row = 0;
   } else if (block >= 3 && block < 6) {
-    index = 1;
+    row = 1;
   } else {
-    index = 2;
+    row = 2;
   }
-  block %= 3;
+  column = block % 3;
 
-  if (this.player == "player1") {
-    this.board[index][block] = "X";
+  if (this.player == "Player 1") {
+    this.board[row][column] = "X";
   } else {
-    this.board[index][block] = "O";
+    this.board[row][column] = "O";
   }
   // checks turn and assigns player
   this.turns++;
   if (this.turns % 2 === 0) {
-    this.player = "player2";
+    this.player = "Player 2";
   } else {
-    this.player = "player1";
+    this.player = "Player 1";
   }
 };
 
-TicTacToe.prototype.winningPlay = function() {
+TicTacToe.prototype.play = function(id) {
+  this.turn(id);
+  this.win();
+  if (this.finish === false) {
+    if (this.turns === 10) {
+      $("#notice").text("It's a draw!");
+    }
+  } else {
+      $("#notice").text(this.player + " wins!");
+    }
+  };
+
+
+TicTacToe.prototype.win = function() {
   // rows
   for (var i = 0; i < this.board.length; i++) {
-    var row = this.board[i];
-    if (row[0] == row[1] && row[1] == row[2]) {
-      this.finish = true;
+    console.log(this.board[0][i]);
+    var row = this.board[0][i];
+    if ((row[0] == row[1]) && (row[1] == row[2])) {
+      // this.finish = true;
     }
+  }
 
     // columns
     var col1 = this.board[0];
     var col2 = this.board[1];
     var col3 = this.board[2];
-    if (col1[i] == col2[i] && col2[i] == col3[i]) {
-      this.finish = true;
+    for (var j = 0; j < this.board.length; j++) {
+    if ((col1[j] == col2[j]) && (col2[j] == col3[j])) {
+      // this.finish = true;
     }
 
     // diagnonals
-    if (col1[0] == col2[1] && col2[1] == col3[2]) {
-      this.finish = true;
+    if ((col1[0] == col2[1]) && (col2[1] == col3[2])) {
+      // this.finish = true;
     }
-    if (col1[2] == col2[1] && col2[1] == col3[0]) {
-      this.finish = true;
+    if ((col1[2] == col2[1]) && (col2[1] == col3[0])) {
+      // this.finish = true;
     }
-}
+  }
 };
