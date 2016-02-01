11e7ba9fbfa8e5d89ffcbd14b078e1fe90c3f26c
Rebecca Tolmach <rmtolmach@gmail.com>
2016-01-31 16:15:46 -0800

    game works and score is kept in an array, but game does not stop once someone has won.

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -3,45 +3,75 @@ var TicTacToe = function() {
  this.board = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
  this.player1 = true;
  this.player2 = false;
+ this.score = [[9, 9, 9], [9, 9, 9], [9, 9, 9]];
 };
 
 
   TicTacToe.prototype.play = function() {
-//when a player
-//how to update the array??
-// how to access board in here.
 //player 1 will be 1s and player 2 will be 2s.
     var self = this;
       self.board.forEach(function(cell) {
         $("#" + cell).click(function() {
-          $("#" + cell).html(self.markScore(), self.changePlayer()
-          );
+          $("#" + cell).html(self.markBoard()); self.scoreGame($(this)); self.changePlayer();
         });
       }); //ends forEach
     };  // ends play function
 
-  TicTacToe.prototype.markScore = function() {
-        if (this.player1) {
-          return "<h1 class='x'>X</h1>";
-        } else {
-          return "<h1 class='o'>O</h1>";
-        }
-      };
+//This function is called in .play
+  TicTacToe.prototype.markBoard = function() {
+    if (this.player1) {
+      return "<h1 class='x'>X</h1>";
+    } else {
+      return "<h1 class='o'>O</h1>";
+    }
+  };
 
+//This function is called in .play
   TicTacToe.prototype.changePlayer = function() {
-        if (this.player1) {
-          this.player1 = false;
-          this.player2 = true;
-        } else {
-          this.player2 = false;
-          this.player1 = true;
-        }
-      };
+    if (this.player1) {
+      this.player1 = false;
+      this.player2 = true;
+    } else {
+      this.player2 = false;
+      this.player1 = true;
+    }
+  };
 
 
+// This function is called in .play (before changePlayer)
+  TicTacToe.prototype.scoreGame = function(clicked) {
+// assign point depending on whose turn it is.
+    var point;
+    if (this.player1) {
+      point = 1;
+    } else {
+      point = 2;
+    }
+// put point in 2D scoring array
+    if (clicked.attr("id") === "one") {
+      this.score[0][0] = point;
+    } else if (clicked.attr("id") === "two") {
+      this.score[0][1] = point;
+    } else if (clicked.attr("id") === "three") {
+      this.score[0][2] = point;
+    } else if (clicked.attr("id") === "four") {
+      this.score[1][0] = point;
+    } else if (clicked.attr("id") === "five") {
+      this.score[1][1] = point;
+    } else if (clicked.attr("id") === "six") {
+      this.score[1][2] = point;
+    } else if (clicked.attr("id") === "seven") {
+      this.score[2][0] = point;
+    } else if (clicked.attr("id") === "eight") {
+      this.score[2][1] = point;
+    } else {
+      this.score[2][2] = point;
+    }
+  };
 
-
-
-
-
-// player 1 and player 2 that switch back and forth
+// decide a winner
+  TicTacToe.prototype.checkWinner = function() {
+// check to see if score has certain patterns in it
+// For example, if there are all 1s this.score[0], player 1 has won.
+// Alert player that they have won.
+  };
